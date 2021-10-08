528986e691e02b3be1bc03d6ce9fc84f748e60e0
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-10-08 20:20:01 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,109 @@
+***targil 8 page 25:
+  
+max_pos = 0
+
+while True:
+    x = int(input("please enter a number"))
+
+    if x <= 0:
+        break
+    else:
+        if x > max_pos:
+            max_pos = x
+
+if max_pos > 0:
+    print(max_pos)
+    
+****targil 9 page 25
+
+x = int(input("please enter a number"))
+
+max = x
+i = 1
+counter = i
+
+while i < 3:
+    x = int(input("please enter a number"))
+    i = i + 1
+    if x > max:
+        max = x
+        counter = i
+
+print(counter)
+
+
+
+****targil 10 page 25
+
+x = int(input("please enter a number"))
+digit = 0
+
+if x < 0:
+    x = x * (-1)
+    
+while True:
+    if x <10:
+        break
+    else:
+        x = x // 10
+
+print(x)
+
+
+****targil 11 page 25
+
+x = int(input("please enter a number"))
+digit = 1
+
+if x < 0:
+    x = x * (-1)
+
+while True:
+    if x < 10:
+        break
+    else:
+        digit += 1
+        x = x // 10
+
+print(digit)
+
+*****targil 12 page 25:
+  
+  x = int(input("please enter a number"))
+sum = 0
+digit = 0
+
+if x < 0:
+    x = x * (-1)
+
+while True:
+    if x > 0:
+        digit = x % 10
+        sum = sum + digit
+        x = x // 10
+    else:
+        break
+
+print(sum)
+
+*****targil 2 page 29
+
+1.  81
+2.  64
+
+
+targil 3 page 29 
+
+
+sum = 0
+
+while True:
+    x = int(input("please enter a number"))
+
+    if x < 0:
+         break
+    else:
+         sum = sum + x
+
+print(sum)
+
